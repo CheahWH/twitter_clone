1cf2ce13ca7d56f5929fb2459706b07fb35bbc9a
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-03-14 04:52:38 -0700

    getting tweets

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Cheah Wen Hui on 14/03/2022.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
