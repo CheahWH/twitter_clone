8ca5e4ac66b95b087cce3ecac5c3560ab310d63b
CheahWH <78494351+CheahWH@users.noreply.github.com>
2022-03-18 07:38:48 -0700

    debugged

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -12,7 +12,7 @@ class TweetViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        tweetTextView.becomeFirstResponder()     
+        tweetTextView.becomeFirstResponder()
     }
     
     @IBOutlet weak var tweetTextView: UITextView!
@@ -24,7 +24,7 @@ class TweetViewController: UIViewController {
     @IBAction func tweet(_ sender: Any) {
         if(!tweetTextView.text.isEmpty)
         {
-            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView, success: {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
                 self.dismiss(animated: true, completion: nil)
             },failure: {(error) in
                 print("Error posting tweet \(error)")
